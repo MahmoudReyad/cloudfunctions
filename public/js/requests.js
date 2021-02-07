a67ab8055da2db231213f90b219c6ecd13de775d
Mahmoud Reyad <m.ryad@more-creatives.com>
2021-02-07 11:12:37 +0200

    Add Vue Instance

--- public/js/requests.js
+++ public/js/requests.js
@@ -1,8 +1,16 @@
-const ref = firebase.firestore().collection('requests');
-ref.onSnapshot(snapshot => {
-    let requests = [];
-    snapshot.forEach(doc => {
-        requests.push({...doc.data() , id: doc.id});
-        console.log(requests);
-    })
-})
+var vue = new Vue({
+    el: '#app',
+    data: {
+        requests: []
+    } , mounted() {
+        const ref = firebase.firestore().collection('requests');
+        ref.onSnapshot(snapshot => {
+            let requests = [];
+            snapshot.forEach(doc => {
+                requests.push({...doc.data() , id: doc.id});
+                console.log(requests);
+            });
+            this.requests = requests;
+        })
+    }
+});
